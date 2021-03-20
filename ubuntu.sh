350a3b20cc2064add677d1a4e1963220fd4dd5fe
Biangkerok32 <48493593+Biangkerok32@users.noreply.github.com>
2021-03-20 07:03:18 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -17,6 +17,7 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+sudo apt install xfce4-terminal
 sudo apt install nautilus nano -y 
 sudo adduser kangris chrome-remote-desktop
 } &> /dev/null &&
